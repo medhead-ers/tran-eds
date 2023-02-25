cbefac0a304c732660587a7be95acb73964e923d
David EVAN <devan.exa@gmail.com>
2023-02-25 16:54:06 +0100

    Mise à jour : Job Mapper

--- src/main/java/com/medhead/ers/tran_eds/application/messaging/service/implementation/JobMapperImpl.java
+++ src/main/java/com/medhead/ers/tran_eds/application/messaging/service/implementation/JobMapperImpl.java
@@ -3,14 +3,20 @@ package com.medhead.ers.tran_eds.application.messaging.service.implementation;
 import com.medhead.ers.tran_eds.application.messaging.event.Event;
 import com.medhead.ers.tran_eds.application.messaging.job.Job;
 import com.medhead.ers.tran_eds.application.messaging.service.definition.JobMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JobMapperImpl implements JobMapper {
+    @Autowired
+    ApplicationContext applicationContext;
     @Override
     public Job createJobFromEvent(Event event) throws Exception {
         Class<?> jobClass = Class.forName(Job.class.getPackageName() + "." + getJobNameFromEvent(event));
-        return (Job) jobClass.getDeclaredConstructor(Event.class).newInstance(event);
+        Job job = (Job) applicationContext.getBean(jobClass);
+        job.setEvent(event);
+        return job;
     }
 
     private String getJobNameFromEvent(Event event){
